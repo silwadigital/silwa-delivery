ca69238e297157346cd8a4e70d3921e19d27dd77
silwadigital <silwa@silwadigital.com.br>
2025-12-30 14:53:52 -0300

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000
+  },
+  // Ensure the base path is correct for Vercel/Netlify (root)
+  base: '/'
+})
